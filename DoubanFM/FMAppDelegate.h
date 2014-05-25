ef4a81c0ea1c20f2e2d5e3f787cefc9638fd2c7b
nirvawolf <nirvawolf@nirvawolfs-MacBook.local>
2014-05-25 15:39:46 +0800

    Initial Commit

---
+++ DoubanFM/FMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FMAppDelegate.h
+//  DoubanFM
+//
+//  Created by nirvawolf on 25/5/14.
+//  Copyright (c) 2014 nirvawolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DoubanFM/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DoubanFM
+//
+//  Created by nirvawolf on 25/5/14.
+//  Copyright (c) 2014 nirvawolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FMAppDelegate class]));
+    }
+}
