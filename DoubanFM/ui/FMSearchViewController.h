c64f932c8e554a6577947a927318a622ef069b7e
nirvawolf <466202783@qq.com>
2014-08-22 16:40:16 +0800

    完善README ，然后添加一个新文件

---
+++ DoubanFM/ui/FMSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  FMSearchViewController.h
+//  DoubanFM
+//
+//  Created by exitingchen on 14-8-22.
+//  Copyright (c) 2014年 nirvawolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMSearchViewController : UIViewController
+
+@end
---
+++ DoubanFM/ui/FMSearchViewController.m
@@ -0,0 +1,38 @@
+//
+//  FMSearchViewController.m
+//  DoubanFM
+//
+//  Created by exitingchen on 14-8-22.
+//  Copyright (c) 2014年 nirvawolf. All rights reserved.
+//
+
+#import "FMSearchViewController.h"
+
+@interface FMSearchViewController ()
+
+@end
+
+@implementation FMSearchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
